255b1995830914728f06c5d2a712a7f01b2bfa81
Daniel Glownia <dglownia222@gmail.com>
2017-01-14 22:21:01 -0600

    add comments

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express')
 const chalk = require('chalk');
 const athenticate = require('./middleware/authenticator');
 const validate = require('./middleware/validator');
+const chatBot = require('./modules/chat-bot');
+const validate = require('./modules/data-repository');
 const app = express()
 
 app.use(athenticate);
@@ -9,13 +11,21 @@ app.use(validate);
 
 // updates the state of the item using a chat bots
 app.post('/text-processor', function(req, res) {
+    var responce  = chatBot.getResponce();
+    //Get state of the window
+    //save data
+    //execute requested action (open/close)
 
-    res.status(202).send(req.body);
+    res.status(201).send("Request has been saved and processed");
 })
 
 //updates the state of the item using amazon alexa
 app.post('/voice-processor', function(req, res) {
-    res.status(202).send(req.body);
+    //Get state of the window
+    //save data
+    //execute requested action (open/close)
+
+    res.status(201).send("Request has been saved and processed");
 })
 
 app.post('/window-state', function(req, res) {
